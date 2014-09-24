58a55ac01c202e745d3f7af44c7a325a6e55ecc0
Ohad Rodeh <orodeh@dnanexus.com>
2014-09-24 12:42:47 -0700

    Improvements all around.

--- src/python/test/file_load/deepdirs/run.sh
+++ src/python/test/file_load/deepdirs/run.sh
@@ -8,11 +8,13 @@ main() {
 
     # create a few subdirectories with data
     mkdir -p $gene_d/clue
-    echo "ABC" > $gene_d/clue/X_1.txt
-    for i in 2 3; do
-        cp $gene_d/clue/X_1.txt $gene_d/clue/X_$i.txt
+    for i in 1 2 3; do
+        echo "ABC" > $gene_d/clue/X_$i.txt
     done
-    cp -r $gene_d/clue $gene_d/hint
+    mkdir -p $gene_d/hint
+    for i in 1 2 3; do
+        echo "hint$i" > $gene_d/hint/V_$i.txt
+    done
 
     # another subdirectory
     phen_d="out/phenotypes"
@@ -20,29 +22,53 @@ main() {
     echo 'hello world' > $phen_d/C.txt
 
     mkdir -p $phen_d/clue2
-    echo "ACGT" > $phen_d/clue2/Y_1.txt
-    for i in 2 3; do
-        cp $phen_d/clue2/Y_1.txt $phen_d/clue2/Y_$i.txt
+    for i in 1 2 3; do
+        echo "ACGT" > $phen_d/clue2/Y_$i.txt
     done
-    cp -r $phen_d/clue2 $phen_d/hint2
+    mkdir -p $phen_d/hint2
+    for i in 1 2 3; do
+        echo "ACGT_$i" > $phen_d/hint2/Z_$i.txt
+    done
+
+    # hidden files, directories, and symbolic links. These should be skipped
+    mkdir $gene_d/.hide
+    echo "XYZ" > $gene_d/.hide/V.txt
+    echo "1234" > $gene_d/.hidden_file
+    ln -s $phen_d/clue $phen_d/clue_symlink
+    ln -s $phen_d/hint $phen_d/hint_symlink
 
     dx-upload-all-outputs
 
     #  Check that directory structure was copied
     #  correctly to the project space
+    mkdir space
+    pushd space
     dx download --overwrite --recursive "/"
+    popd
     mkdir tmp
     mkdir tmp/genes
-    mv {A.txt,B.txt,hint,clue} tmp/genes/
+    mv space/{A.txt,B.txt,hint,clue} tmp/genes/
     mkdir tmp/phenotypes
-    mv {C.txt,hint2,clue2} tmp/phenotypes
+    mv space/{C.txt,hint2,clue2} tmp/phenotypes
 
-    DIFF=$(diff -r tmp out)
-    if [ $DIFF != "" ]
+    # check that hidden files and symbolic links were skipped
+    # the space directory should now be empty
+    extra_files=$(find space)
+    if [ $extra_files != "space" ]
+    then
+        echo "extra files have been uploaded: <$extra_files>"
+        exit 1
+    fi
+
+    # remove the hidden files and symbolic links, so that a recursive diff
+    # will work
+    find out -name ".hid*" | xargs rm -rf
+    find out -name "*symlink" | xargs rm -rf
+
+    DIFF=$(diff -r --brief tmp out)
+    if [ "$DIFF" != "" ]
     then
         echo "Upload of subdirectories does not work properly"
         exit 1
     fi
-
-    # Check that the output json is correct
 }
