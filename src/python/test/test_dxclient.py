6d9d4c1ca055e81e424e1e0c804f197e2ba3070f
Ohad Rodeh <orodeh@dnanexus.com>
2014-10-27 13:46:22 -0700

    revert back to old dxclient.py

--- src/python/test/test_dxclient.py
+++ src/python/test/test_dxclient.py
@@ -505,100 +505,6 @@ class TestDXClient(DXTestCase):
         finally:
             run("dx rmproject -y {p}".format(p=dest_project_id))
 
-    @unittest.skipUnless(testutil.TEST_ENV,
-                         'skipping slow tests')
-    def test_dx_cp_2(self):
-        def create_file_in_project(trg_proj_id):
-            testdir = tempfile.mkdtemp()
-            with tempfile.NamedTemporaryFile(dir=testdir) as fd:
-                fd.write("foo")
-                fd.flush()
-                file_id = run("dx upload {fname} --path {trg_proj} --brief --wait ".
-                              format(trg_proj=trg_proj_id, fname=fd.name)).strip()
-            return file_id
-
-        # setup two projects, source, and target
-        project_name = "test_dx_cp_" + str(random.randint(0, 1000000)) + "_" + str(int(time.time() * 1000))
-        proj_src = run("dx new project {name} --brief".format(name=project_name)).strip()
-        project_name = "test_dx_cp_" + str(random.randint(0, 1000000)) + "_" + str(int(time.time() * 1000))
-        proj_trg = run("dx new project {name} --brief".format(name=project_name)).strip()
-
-        print("copying files 1")
-        for i in range(1,4):
-            file_id = create_file_in_project(proj_src)
-            run("dx cp {f} {p}".format(f=file_id, p=proj_trg))
-
-        # make sure all the files were copied
-        listing_proj1 = run("dx ls --brief {p}".format(p=proj_src))
-        listing_proj2 = run("dx ls --brief {p}".format(p=proj_trg))
-        self.assertEqual(listing_proj1, listing_proj2)
-
-        # create including target path
-        print("copying files to a directory")
-        run("dx mkdir {p}:/foo".format(p=proj_trg))
-        for i in range(1,4):
-            file_id = create_file_in_project(proj_src)
-            run("dx cp {p1}:/{f} {p2}:/foo/".format(p1=proj_src, f=file_id, p2=proj_trg))
-
-        # copy an entire directory
-        print("copying files 2")
-        run("dx cp {p1}:/foo {p2}:/clue".format(p1=proj_trg, p2=proj_src))
-
-        #cleanup
-        run("dx rmproject -y {p1} {p2}".format(p1=proj_src, p2=proj_trg))
-
-
-    @unittest.skipUnless(testutil.TEST_ENV,
-                         'skipping test that would clobber your local environment')
-    def test_dx_cp_no_env(self):
-        ''' Try to copy a file when the context is empty.
-        '''
-        # create a file
-        testdir = tempfile.mkdtemp()
-        with tempfile.NamedTemporaryFile(dir=testdir) as fd:
-            fd.write("foo")
-            fd.flush()
-            file_id = run("dx upload " + fd.name + " --brief --wait").strip()
-            self.assertTrue(file_id.startswith('file-'))
-
-        # Unset environment
-        from dxpy.utils.env import write_env_var
-        write_env_var('DX_PROJECT_CONTEXT_ID', None)
-        del os.environ['DX_PROJECT_CONTEXT_ID']
-        self.assertNotIn('DX_PROJECT_CONTEXT_ID', run('dx env --bash'))
-
-        # Copy the file to a new project.
-        # This should work even though the context is not set.
-        with temporary_project('TestDXClientUploadDownload.test_app1 temporary project') as dxproj:
-            trg_proj_id = dxproj.get_id()
-            run('dx cp ' + file_id + ' ' + trg_proj_id)
-
-    def test_dx_cp_found_in_other_project(self):
-        ''' Copy a file-id, where the file is not located in the default project-id.
-
-        Main idea: create projects A and B. Create a file in A, and copy it to project B,
-        -without- specifying a source project.
-        '''
-        def create_file(trg_proj_id):
-            testdir = tempfile.mkdtemp()
-            with tempfile.NamedTemporaryFile(dir=testdir) as fd:
-                fd.write("foo")
-                fd.flush()
-                file_id = run("dx upload {fname} --path {trg_proj} --brief --wait ".
-                              format(trg_proj=projID1, fname=fd.name)).strip()
-            return file_id
-
-        project_name = "test_dx_cp_" + str(random.randint(0, 1000000)) + "_" + str(int(time.time() * 1000))
-        projID1 = run("dx new project {name} --brief".format(name=project_name)).strip()
-        project_name = "test_dx_cp_" + str(random.randint(0, 1000000)) + "_" + str(int(time.time() * 1000))
-        projID2 = run("dx new project {name} --brief".format(name=project_name)).strip()
-
-        file_id = create_file(projID1)
-        run('dx cp ' + file_id + ' ' + projID2)
-
-        #cleanup
-        run("dx rmproject -y {p1} {p2}".format(p1=projID1, p2=projID2))
-
     def test_dx_gtables(self):
         # new gtable
         gri_gtable_id = run("dx new gtable --gri mychr mylo myhi " +
@@ -1020,7 +926,6 @@ class TestDXClientUploadDownload(DXTestCase):
             output_path = os.path.join(testdir, 'output')
             run('dx download ' + file_id + ' -o ' + output_path)
             run('cmp ' + output_path + ' ' + fd.name)
-
 
     def test_dx_make_download_url(self):
         testdir = tempfile.mkdtemp()
